9e2e08a197f56eca50256d8aa7550fb134506634
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-03-03 14:53:10 -0500

    Creacion de la clase cajero y con su respectivo form.

---
+++ src/main/java/Cajero.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Cajero{
+    public JPanel cajero_panel;
+    private JButton regresarL;
+    private JPanel panel1;
+    private JButton buscar;
+    private JSpinner cantidadSPN;
+    private JButton agregar;
+    private JButton eliminar;
+    private JTable table1;
+    private JTable table2;
+    private JTextField idprodTXT;
+    private JLabel mensajeTXT;
+    private JButton clienteButton;
+    private JButton facturarButton;
+    private JTextField NOMProdTXT;
+    private JTextField precioVenTXT;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
