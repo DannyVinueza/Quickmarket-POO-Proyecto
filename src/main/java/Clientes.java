fe067097d6bd8357fb43ee4af398198bd8646ede
jp123468 <jose.panchi@epn.edu.ec>
2023-03-03 18:32:04 -0500

    Actualizacion del form y java Clientes

--- src/main/java/Clientes.java
+++ src/main/java/Clientes.java
@@ -1,2 +1,49 @@
-public class Clientes {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Clientes extends Login {
+    private JPanel clientes1;
+    private JTextField textCi_Cli;
+    private JTextField textFieldNom_Cli;
+    private JTextField textFieldApell_Cli;
+    private JTextField textFieldCorreo_Cli;
+    private JTextField textFieldTlf_Cli;
+    private JTextField textFieldDirec_Cli;
+    private JLabel Mensajelabel;
+    private JPanel clientes2;
+    private JButton buscar;
+    private JButton agregar;
+    private JButton eliminar;
+    private JButton regresarL;
+    private JButton modificar;
+    private JButton aceptar;
+
+    public Clientes(JFrame client, int ind){
+        super(client);
+        Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("Imagenes/LOGO.png"));
+        setIconImage(img);
+        setLocationRelativeTo(null);
+        setContentPane(clientes1);
+        pack();
+        setVisible(true);
+
+        regresarL.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Cajero cajero = new Cajero(null,ind);
+            }
+        });
+
+        aceptar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Cajero cajero = new Cajero(null,ind);
+            }
+        });
+    }
+
 }
