3d0941e8f238b19a22aad603c31c894f1564af67
Nestor <nestor.chumania@epn.edu.ec>
2023-03-03 15:56:09 -0500

    Funcionalidad de Boton Cajero

--- src/main/java/Cajero.java
+++ src/main/java/Cajero.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Cajero{
+public class Cajero extends Login{
     public JPanel cajero_panel;
     private JButton regresarL;
     private JPanel panel1;
@@ -17,7 +20,34 @@ public class Cajero{
     private JTextField NOMProdTXT;
     private JTextField precioVenTXT;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public Cajero(JFrame cajer, int ind){
+        super(cajer);
+        Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("Imagenes/LOGO.png"));
+        setIconImage(img);
+        setLocationRelativeTo(null);
+        setContentPane(cajero_panel);
+        pack();
+        setVisible(true);
+        regresarL.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                if (ind == 2) {
+                    Login login = new Login(null);
+                } else if(ind == 1) {
+                    //Administrador administrador = new Administrador(null, ind);
+                }
+            }
+        });
+
+        clienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+               // Clientes cliente = new Clientes(null, ind);
+            }
+        });
+
+
     }
 }
