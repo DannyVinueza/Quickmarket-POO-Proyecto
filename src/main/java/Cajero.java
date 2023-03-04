aa30934505bfe6851b864d64af084c7ad938f07b
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-03-03 23:08:12 -0500

    Listener al boton buscar.

--- src/main/java/Cajero.java
+++ src/main/java/Cajero.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.HierarchyBoundsAdapter;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Cajero extends Login{
     public JPanel cajero_panel;
@@ -21,9 +25,20 @@ public class Cajero extends Login{
     private JButton facturarButton;
     private JTextField NOMProdTXT;
     private JTextField precioVenTXT;
+    private JTextField cantidadTF;
 
     private Connection con;
+
     public Cajero(int ind){
+
+        cantidadTF.setEnabled(false);
+        cantidadSPN.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                cantidadTF.setText(cantidadSPN.getValue().toString());
+            }
+        });
+        //Llamada a la pantalla de cajero
         Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("Imagenes/LOGO.png"));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Cajero");
@@ -56,7 +71,25 @@ public class Cajero extends Login{
             @Override
             public void actionPerformed(ActionEvent e) {
                 Conexion conBD = new Conexion();
-                con = conBD.conectar();
+
+                try{
+                    con = conBD.conectar();
+                    String buscar = "SELECT * FROM productos WHERE nombre = '" + idprodTXT.getText() + "'";
+                    Statement stBuscar = con.createStatement();
+                    ResultSet reBuscar = stBuscar.executeQuery(buscar);
+                    if (reBuscar.next()){
+                        if(idprodTXT.getText().equals(reBuscar.getString(2))){
+                            NOMProdTXT.setText(reBuscar.getString(3));
+                            precioVenTXT.setText(reBuscar.getString(4));
+                            cantidadSPN.setValue(reBuscar.getInt(5));
+                        }
+                    }else{
+                        mensajeTXT.setText("No se encuentra el producto!!!!");
+                    }
+                    con.close();
+                }catch (SQLException es){
+                    System.out.println("Se presento un error" + es.getMessage());
+                }
             }
         });
     }
