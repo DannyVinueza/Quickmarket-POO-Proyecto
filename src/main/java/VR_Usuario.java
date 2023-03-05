e0bd21a7df257a49b9a6f99a495173d11762559b
Nestor <nestor.chumania@epn.edu.ec>
2023-03-05 04:28:47 -0500

    Funcionalidad de Tabla de Vistas por Usuarios

--- src/main/java/VR_Usuario.java
+++ src/main/java/VR_Usuario.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class VR_Usuario extends Ventas{
     private JPanel contentPane;
@@ -32,24 +33,49 @@ public class VR_Usuario extends Ventas{
                 Ventas ventas = new Ventas();
             }
         });
+
+        buscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Conexion conBD = new Conexion();
+
+                try{
+                    con = conBD.conectar();
+                    String buscar = "SELECT * FROM usuarios WHERE usuario = '" + usuarioTXT.getText() + "'";
+                    Statement stBuscar = con.createStatement();
+                    ResultSet reBuscar = stBuscar.executeQuery(buscar);
+
+                    if (reBuscar.next()){
+                        mensajeJ.setText("¡Usuario Encontrado!");
+                        llenartabla(usuarioTXT.getText());
+                    }else{
+                        mensajeJ.setText("No se pudo realizar, ¡Usuario no encontrado!");
+                    }
+                    con.close();
+                }catch (SQLException es){
+                    System.out.println("Se presento un error" + es.getMessage());
+                }
+            }
+        });
     }
-    private void llenartabla() {
-        DefaultTableModel modelo = mostrar_VRU();
+    private void llenartabla(String usuario) {
+
+        DefaultTableModel modelo = mostrar_VRU(usuario);
 
         vruTable.setModel(modelo);
     }
 
-    private DefaultTableModel mostrar_VRU()
+    private DefaultTableModel mostrar_VRU(String usuario)
     {
         String [] nombresColumnas = {"N. Factura","Cliente","Cajero","Fecha","Total"};
         String [] registros = new String[5];
 
         DefaultTableModel modelo = new DefaultTableModel(null,nombresColumnas);
-        String sql = "SELECT v.id_venta AS venta_id, c.nombre AS cliente, e.nombre_completo AS empleado, v.fecha_venta, v.importe_total\n" +
+        String sql = "SELECT v.id_venta, c.nombre, e.nombre_completo, v.fecha_venta, v.importe_total\n" +
                 "FROM Facturas v\n" +
                 "JOIN Clientes c ON v.id_cliente = c.id_cliente\n" +
                 "JOIN usuarios e ON v.idusuario = e.idusuario\n" +
-                "where e.usuario = 'danielQ'\n" +
+                "where e.usuario = '" + usuario + "'\n"+
                 "ORDER BY v.fecha_venta DESC;";
         Conexion conBD = new Conexion();
 
